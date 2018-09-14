7d7e28991e34afd69bb90c62fae0799d6770b321
Chancy <chancyleath@hotmail.com>
2018-09-13 20:49:12 -0600

    created placeholders in index, added cdn links, added firebase db to js file, and placeholder

---
+++ assets/javascript/logic.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCjeXJYqaZjLfMdtsDjbG-z33IOHPBFXjE",
+    authDomain: "criminogroupproject.firebaseapp.com",
+    databaseURL: "https://criminogroupproject.firebaseio.com",
+    projectId: "criminogroupproject",
+    storageBucket: "",
+    messagingSenderId: "1067799425974"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+//global variables==============================================================
+
+
+
+
+
+//functions======================================================================
+
+
+
+
+
+//onclick events=================================================================
+
+
+
+})
