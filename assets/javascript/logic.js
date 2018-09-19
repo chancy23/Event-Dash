34deb04dce1a266a3760a6e1600affb8a3dfee2a
Chancy <chancyleath@hotmail.com>
2018-09-18 20:50:30 -0600

    added twitter button to form, added js to get event details to load on new page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-  
+  //materialize js intialization of components
+  //for the modals
+  $("#addEvent, #emptyFieldError").modal();
+
+  //for the floating action button on event card
+  $(".goToDetails").floatingActionButton();
+
+  //for the date picker, set to auto close when date is picked
+  $(".datepicker").datepicker({
+    autoClose: true,
+    format: "m/dd/yy"
+  });
+
+  //for time picker
+  $(".timepicker").timepicker();
 
   // Initialize Firebase
   var config = {
@@ -45,17 +59,10 @@ $(document).ready(function() {
   //when cancel in the form is clicked, reset the form to its original state
   $("#cancelButton").click(resetForm);
 
-  //This opens the modals on the applicable button click
-  $("#addEvent, #emptyFieldError").modal();
-
-  //initialize floating action button on event card (currently not working)
-  // $(".goToDetails").floatingActionButton();
-
   //this is the onclick for when the "Submit" button is pushed from the Add Event form 
   //add input to the DB and create and display the item on the page
   $("#submitEvent").on("click", function(event) {
     event.preventDefault();
-    
     
     var eventTitle = $("#eventTitle").val().trim();
     var date = $("#date").val().trim();
@@ -78,6 +85,8 @@ $(document).ready(function() {
     }
     //if all complete then they can continue (sends info to DB, clears inputs, and closes form)
     else {
+
+      //google API call to get long/lat and add that as a key and property instea of street, city, state,zip
       //create an object to hold all inputs to send to the DB
       var eventInfo = {
         eventTitle: eventTitle,
@@ -94,14 +103,14 @@ $(document).ready(function() {
       database.ref("event/").push(eventInfo);
 
       //testing area
-      console.log("event title: "+ eventTitle);
-      console.log("event date: "+ date);
-      console.log("event time: "+ time);
-      console.log("event street: "+ street);
-      console.log("event city: "+ city);
-      console.log("event state: "+ state);
-      console.log("event zip: "+ zip);
-      console.log("event details: "+ description);
+      console.log("event title: " + eventTitle);
+      console.log("event date: " + date);
+      console.log("event time: " + time);
+      console.log("event street: " + street);
+      console.log("event city: " + city);
+      console.log("event state: " + state);
+      console.log("event zip: " + zip);
+      console.log("event details: " + description);
 
       //reset the form after submit is clicked
       resetForm();
@@ -115,8 +124,8 @@ $(document).ready(function() {
 
     //create a key for the event...the keys in console.log are different than the ones in the DB 
     //and they change every time page is refreshed...why
-    // var eventKey = eventRef.get().key;
-    // console.log(eventKey);
+    var eventKey = snapshotChild.key;
+    // console.log(snapshotChild.key);
 
     //assign the child snapshot values to the  variables
     eventTitle = cv.eventTitle;
@@ -139,11 +148,12 @@ $(document).ready(function() {
     var $eventTitle = $("<span>").addClass("card-title").text(eventTitle);
 
     //add a button to take user to the event detials page....not opening page need to TS with a TA)
-    // var $detailsButton = $("<button>").addClass("btn-floating cyan halfway-fab waves-effect waves-cyan goToDetails");
-    // var $buttonLink = $("<a>").attr("href", "./eventdetails.html");
-    // var $buttonIcon = $("<i>").addClass("material-icons").text("info_outline");
-    // //append link and icon to the button element
-    // $detailsButton.append($buttonLink, $buttonIcon);
+    var $detailsButton = $("<button>").addClass("btn-floating cyan halfway-fab waves-effect waves-cyan goToDetails");
+    $detailsButton.attr("data-id", eventKey);
+    var $buttonLink = $("<a>").attr("href", "./eventdetails.html");
+    var $buttonIcon = $("<i>").addClass("material-icons").text("info_outline");
+    //append link and icon to the button element
+    $detailsButton.append($buttonLink, $buttonIcon);
 
 
     //create a div for the card content, to put some of the info
@@ -153,10 +163,10 @@ $(document).ready(function() {
     // var $time = $("<p>").text(time);
     var $description = $("<p>").text(description);
 
-    //link section to link to event details
-    var $actionDiv = $("<div>").addClass("card-action");
-    var $eventDetails = $("<a>").attr("href", "./eventdetails.html").text("Full Details");
-    $actionDiv.append($eventDetails);
+    //link section to link to event details (currently working on getting button to work)
+    // var $actionDiv = $("<div>").addClass("card-action");
+    // var $eventDetails = $("<a>").attr("href", "./eventdetails.html").text("Full Details");
+    // $actionDiv.append($eventDetails);
 
      //append image and Event title to the image div
      $eventImgDiv.append($eventImg, $eventTitle);
@@ -171,8 +181,8 @@ $(document).ready(function() {
     $eventCard.append(
       $eventImgDiv,
       $eventCardBody,
-      $actionDiv
-      // $detailsButton
+      // $actionDiv
+      $detailsButton
     );
 
     //append the card to the overall event div element
@@ -185,15 +195,28 @@ $(document).ready(function() {
 
   //on click for when user clicks the "info" button on the event card. Goes to Event details page
   //not working I htink I need to add "action" or something to the button when its added
+  // var testID;
+  // console.log(testID);
   $("#allEvents").on("click", ".goToDetails", function(event) {
     event.preventDefault();
+    //opens event details pages
+    window.location.href = "./eventdetails.html";
+    //grab the specific event from the DB
+    var showEventDetail = $(this).attr("data-id");
+    
+    eventRef.ref("/" + showEventDetail).then(function(val) {
+      console.log(val)
+    });
+    // console.log(eventRef);
+    
+      //displays it in the div on the eventsdetails html page
+      $("#eventDetails").text("data-id");
+    });
+
     console.log("event details button clicked");
-  });
+
     
   
-
-
-
 
 
 })
